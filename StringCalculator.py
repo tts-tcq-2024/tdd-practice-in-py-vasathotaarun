f8cd6e48a4b8e4c8c9d07f6d60f56dfbe193de6f
vasathotaarun <vasathotaarun00@gmail.com>
2024-07-19 11:00:14 +0530

    Update StringCalculator.py

--- StringCalculator.py
+++ StringCalculator.py
@@ -1,1 +1,8 @@
-
+import re
+def add(number_string):
+    numbers = re.findall(r'\d+', number_string)
+    number_list = []
+    for number in numbers:
+        if int(number) < 1000:
+            number_list.append(int(number))
+    return sum(number_list)
